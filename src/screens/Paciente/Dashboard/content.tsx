fc32b93b899f07e560c8b47cc05ad0fe29cbd199
DeboraBorgesDev <debora.lara@acad.ufsm.br>
2024-06-18 09:20:56 -0300

    mudança no dashboard de paciente

--- src/screens/Paciente/Dashboard/content.tsx
+++ src/screens/Paciente/Dashboard/content.tsx
@@ -1,43 +1,54 @@
 import React from 'react';
-import { Card, Grid, Typography } from '@mui/material';
+import { Card, Grid, Typography, Avatar } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
 import { useStyles } from './styles';
+import { useOutletContext } from 'react-router-dom';
+import { Paciente } from '../../PacientesList';
 
 const Content: React.FC = () => {
-    const classes = useStyles();
-    return (
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={3}>
-            <Card className={classes.card} style={{backgroundColor: '#19e1af'}}>
-              <Typography variant="h2" align="center">
-                10
-              </Typography>
-              <Typography align="center">
-                Dias em internação
-              </Typography>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Card className={classes.card} style={{backgroundColor: '#19e1af'}}>
-              <Typography variant="h2" align="center">
-                10
-              </Typography>
-              <Typography align="center">
-                Registros de dados vitais
-              </Typography>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Card className={classes.card} style={{backgroundColor: '#19e1af'}}>
-              <Typography variant="h2" align="center">
-                10
-              </Typography>
-              <Typography align="center">
-                Internações
-              </Typography>
-            </Card>
-          </Grid>
+  const classes = useStyles();
+  const {paciente} = useOutletContext<{
+    paciente: Paciente;
+  }>();
+
+  const calcularIdade = (): number => {
+    const diff = Date.now() - new Date(paciente.dataNascimento).getTime();
+    const idadeDate = new Date(diff);
+    return Math.abs(idadeDate.getUTCFullYear() - 1970);
+  };
+
+  return (
+    <>
+      {/* Seção com informações do paciente */}
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card className={classes.card}>
+            <Grid container spacing={2} alignItems="center" justifyContent="space-between">
+              <Grid item sm={4}>
+                <Avatar className={classes.avatar}>
+                  <PersonIcon className={classes.avatarIcon} />
+                </Avatar>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography>
+                  <strong>Nome:</strong> {paciente.nome}
+                </Typography>
+                <Typography>
+                  <strong>Idade:</strong> {calcularIdade()} anos
+                </Typography>
+                <Typography>
+                  <strong>Sexo:</strong> {paciente.sexo === 'f' ? 'Feminino' : 'Masculino'}
+                </Typography>
+                <Typography>
+                  <strong>CPF:</strong> {paciente.cpf}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Card>
         </Grid>
-      );
-    };
+      </Grid>
+    </>
+  );
+};
 
 export default Content;
--- src/screens/Paciente/Dashboard/styles.ts
+++ src/screens/Paciente/Dashboard/styles.ts
@@ -4,13 +4,26 @@ import { createStyles, makeStyles } from "@mui/styles";
 export const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         card: {
-            minHeight: '200px',
-            minWidth: '200px',
             display: 'flex',
             flexDirection: 'column',
-            justifyContent: 'center',
+            justifyContent: 'space-between',
             alignItems: 'center',
             padding: theme.spacing(3)
+        },
+        avatar: {
+            margin: '0 auto',
+            backgroundColor: '#ff6f61',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: theme.spacing(2),
+          },
+          avatarIcon: {
+            fontSize: '6rem',
+            color: '#fff',
+          },
+          margin:{ 
+            marginTop: theme.spacing(2)
         },
     })
 );
